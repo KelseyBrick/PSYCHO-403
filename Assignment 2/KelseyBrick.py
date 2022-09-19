68dc153ead988ce699e98772757bdea6f914d923
KelseyBrick <113373038+KelseyBrick@users.noreply.github.com>
2022-09-19 09:01:38 -0700

    Add files via upload

---
+++ Assignment 2/KelseyBrick.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+print("K")                                          # vertical print    
+print("E")
+print("L")
+print("S")
+print("E")
+print("Y")
+
+print("K", "E", "L", "S", "E", "Y")                 # horizontal print
