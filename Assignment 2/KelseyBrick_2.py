fd4722624bad1ad9b86699a36abe3a1a304b98e1
KelseyBrick <113373038+KelseyBrick@users.noreply.github.com>
2022-09-23 11:33:42 -0700

    Add files via upload

---
+++ Assignment 2/KelseyBrick_2.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#Print Exercises
+
+print("K")                                          # vertical print    
+print("E")
+print("L")
+print("S")
+print("E")
+print("Y")
+
+print("K", "E", "L", "S", "E", "Y")                 # horizontal print
+
+#Variable Exercises
+
+#1. assign the letters as variables
+letter1 = "K"
+letter2 = "E"
+letter3 = "L"
+letter4 = "S"
+letter5 = "E"
+letter6 = "Y"
+
+#2. 
+print(letter1)                                      # vertical print   
+print(letter2)
+print(letter3)
+print(letter4)
+print(letter5)
+print(letter6)
+
+                                                    # horizontal print
+print(letter1, letter2, letter3, letter4, letter5, letter6)
+
+#3. N/A repeated letters in my name.
+
+#4. 
+letterX = "E"
+
+print(letter2)
+print(letter5)
+print(letterX)
+
+print(letter2, letter5, letterX)
+
+#5. 
+letterX = "Q"
+
+print(letter2)
+print(letter5)
+print(letterX)
+
+print(letter2, letter5, letterX)
+
+#6. 
+letterX = letter1
+
+print(letterX)
+print(letter1)
+
+
+letter1 = "Z"
+
+print(letterX)
+print(letter1)
